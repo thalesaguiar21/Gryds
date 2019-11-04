27b3000efdc38b22dfdcc5682598f02d594b2214
Thales Aguiar de Lima <thalesaguiar21@gmail.com>
2019-11-04 10:00:46 -0300

    Changed GrydModel to old-style python interface, so it can be used by pre built packages such as SkLearn

--- gryds/model.py
+++ gryds/model.py
@@ -1,14 +1,9 @@
-from abc import ABC, abstractmethod
-
-
 class GrydModel(ABC):
 
-    @abstractmethod
     def fit(data, **kwargs):
         ''' Train the model with the given data '''
         pass
 
-    @abstractmethod
     def set_parameters(**kwargs):
         ''' Set tunning parameters for this model '''
         pass
