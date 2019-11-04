12c7248a20815e883a38053de4e2b1114e61467d
Thales Aguiar de Lima <thalesaguiar21@gmail.com>
2019-11-04 08:55:32 -0300

    Added method to find files in a given absolute path

---
+++ gryds/file_utils.py
@@ -0,0 +1,24 @@
+import os
+import sys
+
+
+def find_files(path, extension):
+    ''' Given an absolute path, return the absolute path and name of every
+    file in the given path and its subdirectories.
+    '''
+    files = []
+    extension = '.' + extension.lower()
+    for base, directories, fnames in os.walk(path):
+        for fname in fnames:
+            if fname.lower().endswith(extension):
+                files.append(os.path.join(base, fname))
+    return files
+
+
+def find_txt_files(path):
+    return find_files(path, 'txt')
+
+
+def find_lab_files(path):
+    return find_files(path, 'lab')
+
