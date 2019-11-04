c4019823ef3b122bb174a62f8d5ce02de11abb7a
Thales Aguiar de Lima <thalesaguiar21@gmail.com>
2019-11-04 13:33:16 -0300

    ADded unittest for configurations method

--- tests/tests_gridsearch.py
+++ tests/tests_gridsearch.py
@@ -5,7 +5,7 @@ from sklearn.cluster import KMeans
 import numpy as np
 
 from .context import gryds
-from gryds.gridsearch import GS
+from gryds.gridsearch import GS, configurations
 
 
 PATH = os.path.abspath('tests/blobs.txt')
@@ -19,3 +19,12 @@ class TestsGS(unittest.TestCase):
         X, Y = data[:, :-1], data[:, -1]
         gs.tune(KMeans(n_clusters=2), X, Y, n_clusters=[2, 3, 4],
                 max_iter=[100, 200, 300], algorithm=['auto', 'full', 'elkan'])
+
+
+class TestsConfiguration(unittest.TestCase):
+
+    def test_n_configs(self):
+        params = {'a':[1, 2, 3], 'b':['a', 'c'], 'd':[33, 12]}
+        configs = list(configurations(params))
+        self.assertEqual(12, len(configs))
+
