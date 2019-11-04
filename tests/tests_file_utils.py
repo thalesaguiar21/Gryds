cea69a601a1e509f1c4f77633befb11355f1be42
Thales Aguiar de Lima <thalesaguiar21@gmail.com>
2019-11-04 16:30:58 -0300

    Changed import name and added pred_name test

--- tests/tests_file_utils.py
+++ tests/tests_file_utils.py
@@ -2,7 +2,7 @@ import os
 import unittest
 
 from .context import gryds
-from gryds.file_utils import find_files, find_txt_files, find_wav_files, make_pred_name
+from gryds import file_utils as futils
 
 
 PATH = os.path.abspath('tests/test_dir/')
@@ -11,46 +11,53 @@ PATH = os.path.abspath('tests/test_dir/')
 class TestsFindFiles(unittest.TestCase):
 
     def test_nfiles_txt(self):
-        files = find_files(PATH, 'txt')
+        files = futils.find_files(PATH, 'txt')
         self.assertEqual(len(files), 4)
 
     def test_nfiles_wav(self):
-        files = find_files(PATH, 'wav')
+        files = futils.find_files(PATH, 'wav')
         self.assertEqual(len(files), 1)
 
     def test_nfiles_lab(self):
-        files = find_files(PATH, 'lab')
+        files = futils.find_files(PATH, 'lab')
         self.assertEqual(len(files), 0)
 
     def test_unknown_path(self):
         path = ''
-        files = find_files(path, 'txt')
+        files = futils.find_files(path, 'txt')
         self.assertEqual(len(files), 0)
 
     def test_upper_extension(self):
-        files = find_files(PATH, 'TXT')
+        files = futils.find_files(PATH, 'TXT')
         self.assertEqual(len(files), 4)
 
     def test_mixed_case_extension(self):
-        files = find_files(PATH, 'TxT')
+        files = futils.find_files(PATH, 'TxT')
         self.assertEqual(len(files), 4)
 
 
 class TestsFindTxt(unittest.TestCase):
 
     def test_nfiles(self):
-        files = find_files(PATH, 'txt')
-        txt_files = find_txt_files(PATH)
+        files = futils.find_files(PATH, 'txt')
+        txt_files = futils.find_txt_files(PATH)
         self.assertEqual(len(files), len(txt_files))
 
 
 class TestsFindWav(unittest.TestCase):
 
     def test_nfiles(self):
-        files = find_files(PATH, 'wav')
-        wav_files = find_wav_files(PATH)
+        files = futils.find_files(PATH, 'wav')
+        wav_files = futils.find_wav_files(PATH)
         self.assertEqual(len(files), len(wav_files))
 
 
 class TestsPredName(unittest.TestCase):
-    pass
+
+    def test_pred_name(self):
+        config = {'a':'2', 'b':'3'}
+        path = os.path.abspath('tests/')
+        expectedname = f"{path}/a_2_b_3.preds"
+        fname = futils.make_pred_name(path, config)
+        self.assertEqual(expectedname, fname)
+
