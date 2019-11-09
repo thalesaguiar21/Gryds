d51b294c1f61b6309e9e4eed0d3dece18e8d826f
Thales Aguiar de Lima <thalesaguiar21@gmail.com>
2019-11-09 07:07:22 -0300

    Made some atributes private Added verification if the bar has reached 100%, to jump a line

--- gryds/progress_bar.py
+++ gryds/progress_bar.py
@@ -4,31 +4,40 @@ import time
 
 
 class ProgressBar:
+    """
+    Args:
+        n_total
+    """
 
     def __init__(self, n_total, length=20, complete_ch='#', name='Progress'):
-        self.length = length
-        self.n_done = 0
-        self.n_total = n_total
-        self.complete_ch = complete_ch
-        self.remaining_ch = '-'
-        self.name = name
+        self._lenght = length
+        self._n_done = 0
+        self._n_total = n_total
+        self._complete_ch = complete_ch
+        self._remaining_ch = '-'
+        self._name = name
 
     def update(self):
-        self.n_done += 1
-        self.show()
+        self._n_done += 1
+        self._show()
+        if self._is_complete():
+            print()
 
-    def show(self):
+    def _show(self):
         thebar = self._build_bar()
         print(thebar)
         sys.stdout.write('\033[F')
 
     def _build_bar(self):
-        percent_done = self.n_done / self.n_total
-        prop_done = math.ceil(percent_done * self.length)
-        todo = self.length - prop_done
-        done_chs = prop_done * self.complete_ch
-        todo_chs = todo * self.remaining_ch
-        return f"{self.name}: [{done_chs}{todo_chs}]\t{100 * percent_done:3.2f}%"
+        percent_done = self._n_done / self._n_total
+        prop_done = math.ceil(percent_done * self._lenght)
+        todo = self._lenght - prop_done
+        dones = prop_done * self._complete_ch
+        todos = todo * self._remaining_ch
+        return f"{self._name}: [{dones}{todos}]\t{100 * percent_done:3.2f}%"
+
+    def _is_complete(self):
+        return self._n_done == self._n_total
 
 
 if __name__ == '__main__':
