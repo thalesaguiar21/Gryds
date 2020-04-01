6175093d0935796c37ba53071da3670c88557530
Thales Aguiar <thalesaguiar21@gmail.com>
2020-04-01 17:23:39 -0300

    Added docstrings to 'base' module

--- gryds/base.py
+++ gryds/base.py
@@ -4,34 +4,40 @@ from . import confs
 
 
 class GrydModel:
+    """Base interface used by the gsearch module"""
 
     def fit(self, X, Y):
-        ''' Train the model with the given data '''
+        """Train the model with the given data"""
         pass
 
     def predict(self, data):
-        ''' Predict the classes of the given samples '''
+        """Predict the classes of the given samples"""
         pass
 
     def set_params(self, **kwargs):
-        ''' Set tunning parameters for this model '''
+        """Set tunning parameters for this model"""
         pass
 
 
 class Results:
+    """A simple class to hold several results of each type"""
 
     def __init__(self):
         self.scores = []
         self.traintimes = []
         self.testtimes = []
 
-    def add(self, scr, train, test):
-        self.scores.append(scr)
+    def add(self, acc, train, test):
+        self.scores.append(acc)
         self.traintimes.append(train)
         self.testtimes.append(test)
 
 
 def to_saveformat(result):
+    """Converts each result attribute to hold [mean, stdev] and applies any
+    scaling factor defined in 'confs' module
+
+    """
     result.scores = _make_mean_std(result.scores, 100)
     timeunit = confs.get_timeunit()
     result.traintimes = _make_mean_std(result.traintimes, timeunit)
