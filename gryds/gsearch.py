551f04101a9747cc34e6fba5294a3ae2dafba2ec
Thales Aguiar <thalesaguiar21@gmail.com>
2020-05-04 11:19:16 -0300

    Added Y labels to fit function

--- gryds/gsearch.py
+++ gryds/gsearch.py
@@ -48,7 +48,7 @@ def _timed_fit_and_test(model, mselector, X, Y, conf):
         Xtrain, Xtest = X[trn_index], X[tst_index]
         Ytrain, Ytest = Y[trn_index], Y[tst_index]
 
-        trntime, __ = _timeof(model.fit, Xtrain)
+        trntime, __ = _timeof(model.fit, Xtrain, Ytest)
         tsttime, preds = _timeof(model.predict, Xtest)
 
         score = accuracy(preds, Ytest)
